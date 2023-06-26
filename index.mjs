aa0d3b395c27cc3d6fc4c8ae1dfa08bdbd9334dc
Krish Joshi <krish.joshi@tesco.com>
2023-06-26 23:38:01 +0100

    fixed the index runner

--- index.mjs
+++ index.mjs
@@ -1,10 +1,12 @@
 import fs from 'fs'
 import MartianRobot from './MartianRobots/index.mjs'
-// read file from argument and print it to console
+
+// read file from argument and pass it to MartianRobot
 // Example: node index.js test.txt
 const filename = process.argv[2]
 fs.readFile(filename, 'utf8', (err, data) => {
   if (err) throw err
   const martianRobot = new MartianRobot(data)
+  console.log(martianRobot.getRobotState())
 })
 
